51272cbeac0f7fba17096fe4ce6156b4948beba4
ngavinsir <ngavinsir@gmail.com>
2020-03-05 00:05:41 +0700

    get headline

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Auth from "./Auth.svelte";
+	import Headline from "./Headline.svelte"
 </script>
 
 <main>
 	<Auth />
+	<Headline />
 </main>
--- src/Headline.svelte
+++ src/Headline.svelte
@@ -1,11 +1,33 @@
 <script>
     import axios from "axios";
+    import { jwt } from "./stores.js";
+
+    let headline = {};
+
+    $: getHeadline($jwt);
 
     const baseUrl = "http://localhost:4040";
 
-    async function getHeadline() {
+    async function getHeadline(jwt) {
+        if(!jwt) return;
         const url = `${baseUrl}/headline/random`;
-        const { data: headline } = await axios.get(url)
+        const { data } = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        })
+        headline = data;
     }
 
 </script>
+
+<span>{headline.value}</span>
+
+<style>
+span {
+    position: fixed;
+    top: 100px;
+    color: white;
+    font-size: 36px;
+}
+</style>
