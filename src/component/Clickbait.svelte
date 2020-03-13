dbc023ea7ea70e4be77d1c5ae42f35db2031ab9d
ngavinsir <ngavinsir@gmail.com>
2020-03-13 16:46:29 +0700

    disable clickbait  button on loading

--- src/component/Clickbait.svelte
+++ src/component/Clickbait.svelte
@@ -2,14 +2,14 @@
     <button 
         class="bt gray hover:bg-dark-5 w-32 sm:w-40 m-2 sm:mx-6 flex items-center justify-center" 
         on:click={() => sendLabel("NotClickbait")} 
-        disabled={!article}
+        disabled={!article || loading}
     >
         Not Clickbait
     </button>
     <button 
         class="bt white w-32 sm:w-40 m-2 sm:mx-6 flex items-center justify-center" 
         on:click={() => sendLabel("Clickbait")} 
-        disabled={!article}
+        disabled={!article || loading}
     >
         Clickbait
     </button>
@@ -22,26 +22,35 @@
     import { jwt, history, article } from "../stores.js";
     import config from "../config.js";
 
+    let loading = false;
+
     async function sendLabel(label) {
+        loading = true;
         const url = `${config.baseUrl}/labeling/clickbait`;
         const data = {
             value: label,
             article_id: $article.id
         };
-        const { data: { error, label_id, new_article } } = await axios.post(url, data, {
-            headers: {
-                Authorization: `Bearer ${$jwt}`
-            }
-        })
-        if(!error)  { 
-            history.add({ 
-                id: label_id,
-                article: $article,
-                label_value: label,
-                label_updated_at: new Date().toISOString()
+        try {
+            const { data: { error, label_id, new_article } } = await axios.post(url, data, {
+                headers: {
+                    Authorization: `Bearer ${$jwt}`
+                }
             })
+            if(!error)  { 
+                history.add({ 
+                    id: label_id,
+                    article: $article,
+                    label_value: label,
+                    label_updated_at: new Date().toISOString()
+                })
+                $article = new_article;
+            }
+        } catch (e) {
+            // handle send label error
+        } finally {
+            loading = false;
         }
-        $article = new_article;
     }
 
     async function handleKeydown(event) {
