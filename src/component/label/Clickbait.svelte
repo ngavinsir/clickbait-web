7f83edd609454f2cce513fa80d70fd7e1e803729
ngavinsir <ngavinsir@gmail.com>
2020-03-14 18:49:29 +0700

    fix label delete icon flex shrink

--- src/component/label/Clickbait.svelte
+++ src/component/label/Clickbait.svelte
@@ -1,6 +1,6 @@
 <div class={`flex items-center ${wrapperClass(data.label.value)} rounded-lg shadow-xl px-4 py-4`}>
     <span class="value text-left flex-grow leading-none">{data.article.headline}</span>
-    <svg class="cursor-pointer ml-4 group" on:click={() => dispatch('delete', data.label.id)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="flex-shrink-0 cursor-pointer ml-4 group" on:click={() => dispatch('delete', data.label.id)} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class={`stroke-current ${deleteIconClass(data.label.value)}`} d="M19 7L18.1327 19.1425C18.0579 20.1891 17.187 21 16.1378 21H7.86224C6.81296 21 5.94208 20.1891 5.86732 19.1425L5 7M10 11V17M14 11V17M15 7V4C15 3.44772 14.5523 3 14 3H10C9.44772 3 9 3.44772 9 4V7M4 7H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
 </div>
