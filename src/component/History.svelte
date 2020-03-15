168bd29f5cd54d34383584910fcb0f671758ed89
ngavinsir <ngavinsir@gmail.com>
2020-03-16 01:07:24 +0700

    [History.svelte] add loading indicator

--- src/component/History.svelte
+++ src/component/History.svelte
@@ -1,34 +1,58 @@
 {#if $type === "clickbait" || $showHistory}
     <div class="flex flex-col">
-        {#each sortedHistories as history,i (history.label.id)}
-            <div 
-                in:fly={{duration:150, y:50}}
-                out:fly={{duration:150, y:50}}
-                animate:flip={{duration:150}} class="mb-4 last:mb-0">
-                <svelte:component 
-                    this={labelComponent} 
-                    data={history} 
-                    on:delete={e => deleteLabel(e.detail)}
-                    deleting={deleting.includes(history.label.id)}
+        {#if sortedHistories.length}
+            {#each sortedHistories as history,i (history.label.id)}
+                <div 
+                    in:fly={{duration:150, y:50}}
+                    out:fly={{duration:150, y:50}}
+                    animate:flip={{duration:150}} class="mb-4 last:mb-0">
+                    <svelte:component 
+                        this={labelComponent} 
+                        data={history} 
+                        on:delete={e => deleteLabel(e.detail)}
+                        deleting={deleting.includes(history.label.id)}
+                    />
+                </div>
+            {/each}
+        {:else if loading}
+            <div class="py-4 self-center">
+                <Spinner  
+                    size="50"
+                    speed="750"
+                    color="#41da8f"
+                    thickness="3"
+                    gap="40"
                 />
             </div>
-        {/each}
+        {:else}
+            <span
+                in:fly={{duration:150, y:50}}
+                out:fly={{duration:150, y:50}}
+                class="self-center py-4 text-white font-bold opacity-50 text-lg"
+            >
+                NO HISTORY
+            </span>
+        {/if}
     </div>
 {/if}
 
 <svelte:window on:keydown={handleKeydown}/>
 
 <script>
+    import cancel from "axios";
     import _ from "lodash";
     import ClickbaitLabel from "./label/Clickbait.svelte";
     import SummaryLabel from "./label/Summary.svelte";
+    import Spinner from 'svelte-spinner';
     import { history, type, showHistory } from "../stores.js";
     import { fly } from 'svelte/transition';
     import { flip } from "svelte/animate"
-    import { getContext } from "svelte";
+    import { getContext, tick } from "svelte";
 
     const { axios } = getContext("axios");
     let deleting = [];
+    let loading = false;
+    let cancelSource = null;
 
     $: getHistories($type);
     $: sortedHistories = _.sortBy($history, function(label) {
@@ -37,10 +61,27 @@
     $: labelComponent = $type == "clickbait" ? ClickbaitLabel : $type == "summary" ? SummaryLabel : null;
 
     async function getHistories(type) {
+        if (cancelSource) cancelSource.cancel();
+        const timeout = setTimeout(() => {
+            loading = true
+        }, 100);
+        cancelSource = cancel.CancelToken.source();
         $history = [];
+        await tick();
         const url = `/${type}/label`;
-        const { data: histories, data: { error } } = await axios.get(url);
-        if(!error) $history = histories;
+        try {
+            const { data: histories, data: { error } } = await axios.get(url, {
+                cancelToken: cancelSource.token
+            });
+            if(!error) $history = histories;
+        } catch (e) {
+            console.log(e);
+            // handle get histories error
+        } finally {
+            clearTimeout(timeout);
+            loading = false;
+            cancelSource = null;
+        }
     }
 
     async function deleteLabel(labelID) {
