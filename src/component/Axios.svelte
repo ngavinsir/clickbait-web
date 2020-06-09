000cb906cfa43e562429884b4c6656e676f28655
ngavinsir <ngavinsir@gmail.com>
2020-06-09 18:55:40 +0700

    axios: show error notification on timeout

--- src/component/Axios.svelte
+++ src/component/Axios.svelte
@@ -1,18 +1,31 @@
 <script>
-    import { setContext } from 'svelte';
+    import { setContext, getContext } from 'svelte';
     import { jwt } from "../stores.js";
     import axios from "axios";
     import config from "../config.js";
+    
+    const { show: showNotification } = getContext("notification");
 
     $: instance.defaults.headers.common['Authorization'] = `Bearer ${$jwt}`;
 
     const instance = axios.create({
-            baseURL: config.baseUrl,
-            headers: {
-                Authorization: `Bearer ${$jwt}`
+      baseURL: config.baseUrl,
+      headers: {
+        Authorization: `Bearer ${$jwt}`
 	    },
-	    timeout: 2000
-        });
+      timeout: 3000
+    });
+    instance.interceptors.response.use(
+      config => {
+        return config;
+      },
+      (error) => {
+        if (error.code === 'ECONNABORTED') {
+          showNotification("Tidak dapat menghubungi server.", 3000);
+        }
+        return Promise.reject(error);
+      },
+    );
     
     setContext("axios", {
         axios: instance
